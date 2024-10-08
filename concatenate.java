a3cef72675350c95b56e5be72384b43167acada3
alexlangtabi <68006914+alexlangtabi@users.noreply.github.com>
2024-10-08 09:17:30 +0000

    Concatenate String and Value

---
+++ concatenate.java
@@ -0,0 +1,16 @@
+public class concatenate {
+    public static void main(String[] args) {
+        float num1 = 2.0F;
+        boolean a = true;
+        int number = 3110;
+        byte zero = 0;
+        byte one = 1;
+               
+        System.out.println("H"+number+" " + "w" +zero + "r"+one+"d " + num1 + " " + a);
+
+
+    }
+
+}
+
+
